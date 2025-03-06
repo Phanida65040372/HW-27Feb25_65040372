8a515caf694ef9fd08669db0406f5a61dff9c342
Phanida65040372 <65040372@go.buu.ac.th>
2025-03-06 15:06:03 +0700

    Add files via upload

---
+++ HW 27Feb25_65040372.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string readtext = input1_TextBox.Text;
+            string[] twonumbers = readtext.Split('x');
+            double a, b, c;
+            a = Convert.ToDouble(twonumbers[0]);
+            b = Convert.ToDouble(twonumbers[1]);
+            c = a * b;
+            solution_label.Text = c.ToString();
+            solution_textbox.Text = c.ToString();
+        }
+    }
+}
